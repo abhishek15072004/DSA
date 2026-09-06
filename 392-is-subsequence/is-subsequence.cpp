3042c4a75e0ab478706e71497b7a57ca8d1f5b7d
abhishek15072004 <akrgupta30@gmail.com>
2026-09-06 22:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.95%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-inline static int t[10001][10001];
-int lcs(string& a,string& b,int n,int m){
-    if(n==0 || m==0){
-        return 0;
-    }
-    if(t[n][m]!=-1){
-        return t[n][m];
-    }
-    if(a[n-1]==b[m-1]){
-        return t[n][m]=1+lcs(a,b,n-1,m-1);
-    }else{
-        return t[n][m]= max(lcs(a,b,n-1,m),lcs(a,b,n,m-1));
-    }
-    return t[n-1][m-1];
-}
-    bool isSubsequence(string s, string k) {
-        memset(t,-1,sizeof(t));
-        int n=k.length();
-        int m=s.length();
-        return lcs(k,s,n,m)==m;
+
+    bool isSubsequence(string s, string t) {
+      int i=0, j=0;
+      while(i<s.size() && j<t.size()){
+        if(s[i]==t[j]){
+            i++;
+        }
+        j++;
+      }
+      return i==s.size();
     }
 };
